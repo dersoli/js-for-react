10056fdcff603d6186295e327298eb66dbe965e7
Anderson Xavier <dersoli@gmail.com>
2023-10-09 22:40:14 -0300

    image added to CardGame.js

--- matching-game/src/components/CardGame/index.js
+++ matching-game/src/components/CardGame/index.js
@@ -1,7 +1,9 @@
+import './style.css';
+
 function CardGame() {
     return`
-        <article>
-            CardGame
+        <article class='card-game'>
+            <img src='../../images/alura-pixel.png' alt='Logo da Alura'>
         </article>
     `
 }
