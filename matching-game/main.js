db778696ab2c2ae6b84714c6cc4fd53ce8b63802
Anderson Xavier <dersoli@gmail.com>
2023-10-09 22:55:07 -0300

    PlayerName added to main.js

--- matching-game/main.js
+++ matching-game/main.js
@@ -2,10 +2,14 @@ import './src/styles/settings/colors.css';
 import './src/styles/elements/base.css';
 import './src/styles/generic/reset.css';
 import CardGame from "./src/components/CardGame";
+import PlayerName from './src/components/PlayerName';
 
 const $root = document.querySelector('#root');
 
 const $htmlCardGame = CardGame();
 
+const $htmlPlayerName = PlayerName("Player 1")
+
 console.log($htmlCardGame);
 $root.insertAdjacentHTML('beforeend', $htmlCardGame);
+$root.insertAdjacentHTML('afterbegin', $htmlPlayerName);
