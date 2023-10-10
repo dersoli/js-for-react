b2112fd262ee3971d1cf35c89d31721951925bed
Anderson Xavier <dersoli@gmail.com>
2023-10-09 22:39:51 -0300

    import some archives to main.js

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,3 +1,6 @@
+import './src/styles/settings/colors.css';
+import './src/styles/elements/base.css';
+import './src/styles/generic/reset.css';
 import CardGame from "./src/components/CardGame";
 
 const $root = document.querySelector('#root');
