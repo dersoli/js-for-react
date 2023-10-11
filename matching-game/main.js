4ba9c3d844f2ff78e30607ff45d092d0ee8c9cde
Anderson Xavier <dersoli@gmail.com>
2023-10-11 19:09:08 -0300

    PlayerName removed of main.js

--- matching-game/main.js
+++ matching-game/main.js
@@ -1,20 +1,15 @@
 import './src/styles/settings/colors.css';
 import './src/styles/elements/base.css';
 import './src/styles/generic/reset.css';
-import PlayerName from './src/components/PlayerName';
 import BoardGame from './src/objects/BoardGame';
+import ScoreBoard from './src/objects/BoardGame/ScoreBoard';
 
 const $root = document.querySelector('#root');
 
-const $htmlBoardGame = BoardGame(6);
-
-const $htmlPlayer1Name = PlayerName("Player 1");
-const $htmlPlayer2Name = PlayerName("Player 2");
 $root.insertAdjacentHTML(
     'beforeend',
     `
-        ${PlayerName("Player1")}
-        ${PlayerName("Player2")}
+        ${ScoreBoard()}
         ${BoardGame(6)}
     `
     );
